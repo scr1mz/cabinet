5c83b3c331e042ba665950aa0dd52409caafeb15
scrimz <isakov.kostik@outlook.com>
2023-12-30 16:00:04 +0300

    cabinet v1

---
+++ src/composables/useAPIFetch.ts
@@ -0,0 +1,24 @@
+import { stringify } from "qs";
+import { useFetch, useRuntimeConfig } from "nuxt/app";
+
+type useFetchType = typeof useFetch;
+
+// wrap useFetch with configuration needed to talk to our API
+export const useAPIFetch: useFetchType = (path, options = {}) => {
+  const config = useRuntimeConfig();
+
+  options.server = false;
+
+  if (import.meta.env.MODE && config.public.devToken) {
+    options.query = {
+      dev_token: config.public.devToken,
+      ...options.query
+    };
+  }
+
+  const { query, ...optionsWithoutQuery } = options;
+
+  const pathWidthQuery = query ? `${path}?${stringify(query)}` : path;
+
+  return useFetch(pathWidthQuery, optionsWithoutQuery);
+};
